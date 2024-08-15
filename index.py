96b8070c60c242e68ed964a8686b4cfd6ad36799
kat coveney <katcoveney.author@gmail.com>
2024-08-15 11:58:57 -0500

    added the str method and when the animals were fed.

--- index.py
+++ index.py
@@ -2,26 +2,46 @@ from walking import Donkey, Llama, Goat, Sheep, Pig
 from slithering import Copperhead, RatSnake, CornSnake, BallPython, KingSnake
 from swimming import Mallard, Goldfish, Koi, Turtle, Frog
 
-# Creating instances of each class with a shift for walking animals
-miss_fuzz = Llama(name="Miss Fuzz", species="Llama", shift="morning")
-billy = Goat(name="Billy", species="Goat", shift="midday")
-charlie = Donkey(name="Charlie", species="Donkey", shift="afternoon")
-wooly = Sheep(name="Wooly", species="Sheep", shift="morning")
-porky = Pig(name="Porky", species="Pig", shift="midday")
+# Creating instances of each class with a shift and food for walking animals
+miss_fuzz = Llama(name="Miss Fuzz", species="Llama", shift="morning", food="Llama Chow")
+billy = Goat(name="Billy", species="Goat", shift="midday", food="Hay")
+charlie = Donkey(name="Charlie", species="Donkey", shift="afternoon", food="Carrots")
+wooly = Sheep(name="Wooly", species="Sheep", shift="morning", food="Grass")
+porky = Pig(name="Porky", species="Pig", shift="midday", food="Pig Feed")
 
 # Creating instances of slithering animals
-slither = Copperhead(name="Slither", species="Copperhead")
-sneaky = RatSnake(name="Sneaky", species="Rat Snake")
-stripey = CornSnake(name="Stripey", species="Corn Snake")
-curly = BallPython(name="Curly", species="Ball Python")
-king = KingSnake(name="King", species="King Snake")
+slither = Copperhead(name="Slither", species="Copperhead", food="Mice")
+sneaky = RatSnake(name="Sneaky", species="Rat Snake", food="Rats")
+stripey = CornSnake(name="Stripey", species="Corn Snake", food="Crickets")
+curly = BallPython(name="Curly", species="Ball Python", food="Rats")
+king = KingSnake(name="King", species="King Snake", food="Mice")
 
 # Creating instances of swimming animals
-quackers = Mallard(name="Quackers", species="Mallard")
-bubbles = Goldfish(name="Bubbles", species="Goldfish")
-splash = Koi(name="Splash", species="Koi")
-shellie = Turtle(name="Shellie", species="Turtle")
-ribbit = Frog(name="Ribbit", species="Frog")
+quackers = Mallard(name="Quackers", species="Mallard", food="Duck Feed")
+bubbles = Goldfish(name="Bubbles", species="Goldfish", food="Fish Flakes")
+splash = Koi(name="Splash", species="Koi", food="Koi Pellets")
+shellie = Turtle(name="Shellie", species="Turtle", food="Turtle Pellets")
+ribbit = Frog(name="Ribbit", species="Frog", food="Insects")
+
+# Feeding the animals
+miss_fuzz.feed()
+billy.feed()
+charlie.feed()
+wooly.feed()
+porky.feed()
+
+slither.feed()
+sneaky.feed()
+stripey.feed()
+curly.feed()
+king.feed()
+
+quackers.feed()
+bubbles.feed()
+splash.feed()
+shellie.feed()
+ribbit.feed()
+
 
 # Printing the name of each critter to verify
 print(miss_fuzz)
@@ -30,14 +50,14 @@ print(charlie)
 print(wooly)
 print(porky)
 
-print(slither.name)
-print(sneaky.name)
-print(stripey.name)
-print(curly.name)
-print(king.name)
+print(slither)
+print(sneaky)
+print(stripey)
+print(curly)
+print(king)
 
-print(quackers.name)
-print(bubbles.name)
-print(splash.name)
-print(shellie.name)
-print(ribbit.name)
+print(quackers)
+print(bubbles)
+print(splash)
+print(shellie)
+print(ribbit)
--- slithering/ratsnake.py
+++ slithering/ratsnake.py
@@ -1,8 +1,16 @@
 from datetime import date
 
 class RatSnake:
-        def __init__(self, name, species):
-            self.name = name
-            self.species = species
-            self.date_added = date.today()
-            self.slithering = True
+    def __init__(self, name, species, food):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}."
