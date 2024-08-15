beccbf09720e210c8f2a3f6554ade484b2af1c37
kat coveney <katcoveney.author@gmail.com>
2024-08-15 11:36:23 -0500

    added the shifts to the walking animal classes.

--- walking/sheep.py
+++ walking/sheep.py
@@ -1,8 +1,12 @@
 from datetime import date
 
 class Sheep:
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
+
+    def __str__(self):
+        return f"{self.name} the {self.species} is available to pet during the {self.shift} shift."
